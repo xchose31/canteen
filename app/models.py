953d4aaffb8a4d40d8e2ee7251357e0bfeceb72e
xchose31 <rte3001@vk.com>
2026-01-19 22:38:57 +0300

    initial commit, User model added

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+Base = db.Model
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(80), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password_hash = Column(String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(password, self.password_hash)
+
