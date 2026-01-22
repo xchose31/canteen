3529c925c259151cf68c4e95dddf2e673ded356f
Honor <rte3001@vk.com>
2026-01-22 15:44:10 +0300

    добавлена модель блюда (dish)

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, JSON
 from sqlalchemy.orm import relationship
 from app import db
+import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 Base = db.Model
@@ -24,3 +25,12 @@ class User(Base):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.role}')"
 
+class Dish(Base):
+    __tablename__='dishes'
+
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(80), nullable=False)
+    weight = Column(Integer, nullable=False)
+    cost= Column(Integer, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.datetime.now())
+    characteristics = Column(JSON, nullable=False)
